b4e14989b60761b8d03273032cacf08f6e29a510
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2022-12-08 12:16:57 -0300

    Implementing BookRepository in BookService

--- src/main/java/com/api/bookapi/services/BookService.java
+++ src/main/java/com/api/bookapi/services/BookService.java
@@ -1,6 +1,12 @@
 package com.api.bookapi.services;
 
+import com.api.bookapi.repositories.BookRepository;
 import org.springframework.stereotype.Service;
 @Service
 public class BookService {
+    final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 }
