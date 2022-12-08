1c35dc23d74c63afd49b5348191e3ab931489520
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2022-12-08 11:55:26 -0300

    Implementing getters and setters of BookModel

--- src/main/java/com/api/bookapi/models/BookModel.java
+++ src/main/java/com/api/bookapi/models/BookModel.java
@@ -23,4 +23,51 @@ public class BookModel implements Serializable {
     @Column(nullable = false)
     private int releaseYear;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPageQty() {
+        return pageQty;
+    }
+
+    public void setPageQty(int pageQty) {
+        this.pageQty = pageQty;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
 }
